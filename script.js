bce045cc4a990c462577d91ca7ac907cdf432634
Rodrigo Santos da Silva <rodrigonst3@gmail.com>
2018-06-05 21:29:18 -0300

    organização do repositório

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ function myFunction(){
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(event){
-    if(!event.target.matches(".menuDropdownBotao")){
-
-        var dropdowns = document.getElementsByClassName("menuDropdownConteudo");
-        var i;
-        for(i = 0; i < dropdowns.length; i++){
-            var openDropdown = dropdowns[i];
-            if(openDropdown.classList.contains('show')){
-                openDropdown.classList.remove('show');
-            }
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
         }
     }
 }
