b3bb0a429747ae571e0fbf8a651983a239ba6bdb
paodealho404 <phdbnpedro@gmail.com>
2018-06-23 12:10:01 -0300

    nada de diferente, somente alterações visuais, nenhuma alteração funcional

--- encontre_duo/src/components/cadastro.js
+++ encontre_duo/src/components/cadastro.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './cadastro.css';
 
 export default class Cadastro extends Component {
     render() {
         return (
             <div>
-                <h1>Home!</h1>
+                <h1>Tela de cadastro</h1>
                 <div>
-                    <Link to="/">Home</Link>
+                    <Link to="/">Pagina inicial</Link>
                 </div>
                 
             </div>
