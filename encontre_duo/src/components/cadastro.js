f9c5d3fca20a2e17bb3af91352d81a149e7f0a0b
paodealho404 <phdbnpedro@gmail.com>
2018-06-23 11:38:23 -0300

    uma tela de cadastro ta aí, os links tão funcionando, nada na tela de cadastro pronto ainda

---
+++ encontre_duo/src/components/cadastro.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Cadastro extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Home!</h1>
+                <div>
+                    <Link to="/">Home</Link>
+                </div>
+                
+            </div>
+            );
+    }
+
+
+}
