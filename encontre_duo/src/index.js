d02f6ed0ff6ed777dd19efe598a1a92f43d61e6b
Rodrigo Santos da Silva <rodrigonst3@gmail.com>
2018-06-25 19:39:36 -0300

    Remoção de arquivos desnecessários

--- encontre_duo/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Duo from './duo_page';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Duo />, document.getElementById('root'));
-registerServiceWorker();
