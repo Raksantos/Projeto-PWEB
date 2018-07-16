3872a3b1c46870876adf023dfce610245b2a05e4
Joao Falcao <joao.falcao@icloud.com>
2018-07-16 20:52:55 -0300

    validação do formulário

--- encontre_duo/src/components/tela_cadastro.js
+++ encontre_duo/src/components/tela_cadastro.js
@@ -7,20 +7,49 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 export default class Cadastro extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
+        this.state = { email: '', senha: '', descricao: ''};
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ value: event.target.value });
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({ [name]: value });
     }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+        if(!this.checkSenha(this.state.senha))
+            alert('insira uma senha válida (6 a 12 dígitos, sem espaços)');
+        else if(!this.checkString(this.state.email))
+            alert('insira um email válido (no máx. 40 caracteres)');
+        else if(this.state.descricao.length > 500)
+            alert('descrição longa demais');
+        else            
+            alert('veio do form: ' + this.state.email + " " + this.state.descricao + " " + this.state.senha);
         event.preventDefault();
     }
+
+    checkString(str){
+        if(str.replace(/\s/g,"") == "")
+            return false;
+        else if(str.length > 40)
+            return false;
+        else
+            return true;          
+    }
+
+    checkSenha(str){
+        if(str.replace(/\s/g,"") == "")
+            return false;
+        else if(str.length > 12 || str.length < 6)
+            return false;
+        else
+            return true;
+    }
+
     render() {
         return (
 
@@ -30,11 +59,11 @@ export default class Cadastro extends Component {
                         <div className="card w-50 m-5">
                             <div class="card-header">Cadastro de Usuário</div>
                             <div className="card-body">
-                                <Form action="http://localhost:8000/" method="POST" onSubmit={this.handleSubmit} >
+                                <Form method="POST" onSubmit={this.handleSubmit} >
                                     <div className="form-group">
                                         <FormGroup>
                                             <Label for="exampleEmail" className="">Email</Label>
-                                            <Input type="email" name="email" id="exampleEmail" placeholder="email@exemplo.com" onChange={this.handleChange} />
+                                            <Input type="email" name="email" id="email" placeholder="email@exemplo.com" onChange={this.handleChange} />
                                         </FormGroup>
                                     </div>
                                     <div className="form-group">
@@ -42,13 +71,13 @@ export default class Cadastro extends Component {
                                             <div className="col-md-6">
                                                 <FormGroup>
                                                     <Label for="examplePassword" className="">Senha</Label>
-                                                    <Input type="password" name="password" id="examplePassword" placeholder="Senha" onChange={this.handleChange} />
+                                                    <Input type="password" name="senha" id="senha" placeholder="Senha" onChange={this.handleChange} />
                                                 </FormGroup>
                                             </div>
                                             <div className="col-md-6">
                                                 <FormGroup>
                                                     <Label for="confirmPassword" className="">Confirmação de Senha</Label>
-                                                    <Input type="password" name="confirmpassword" id="confirmPassword" placeholder="Confirmação" onChange={this.handleChange} />
+                                                    <Input type="password" name="confirm" id="confirm" placeholder="Confirmação" onChange={this.handleChange} />
                                                 </FormGroup>
                                             </div>
                                         </div>
@@ -56,7 +85,7 @@ export default class Cadastro extends Component {
                                     <div className="form-group">
                                         <FormGroup>
                                             <Label for="exampleText" className="">Descrição</Label>
-                                            <Input type="textarea" name="text" id="exampleText" placeholder="Escreva um pouco sobre você" onChange={this.handleChange} />
+                                            <Input type="textarea" name="text" name="descricao" id="descricao" placeholder="Escreva um pouco sobre você" onChange={this.handleChange} />
                                         </FormGroup>
                                     </div>
 
