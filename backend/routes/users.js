7a267917e572c9ce410f3c40d7f9a6580ea50892
Joao Falcao <joao.falcao@icloud.com>
2018-09-08 20:20:31 -0300

    user login

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -54,20 +54,80 @@ users.post('/cadastrar', function (req, res) {
 });
 
 users.get('/logar', function (req, res) {
-    var user = req.body;
-    delete user.redirect;
-    console.log(user);
-    con.query('select ? from t_usuario ', user, function (err, result) {
+
+    var resposta = {};
+    var email = req.body.email;
+    var senha = req.body.senha;
+
+    database.connection.getConnection(function (err, connection) {
         if (err) {
-            res.send("erro sql");
-            throw err;
+            resposta["erro"] = 1;
+            resposta["dados"] = "Erro Interno do Servidor";
+            res.json(resposta);
+        } else {
+            connection.query('SELECT * FROM t_usuario WHERE email = ?', email, function (err, rows, fields) {
+                if (err) {
+                    resposta["erro"] = 1;
+                    resposta["dados"] = "Erro SQL";
+                    res.json(resposta);
+                } else {
+                    if (rows.length > 0) {
+                        var hash = rows[0].senha;
+                        bcrypt.compare(senha, hash, function (err, res) {
+                            if (res == true) {
+                                token = jwt.sign(rows[0], process.env.SECRET_KEY, {
+                                    expiresIn: "3h"
+                                });
+                                resposta["erro"] = 0;
+                                resposta["token"] = token;
+                                resposta["dados"] = "Logado com sucesso";
+                                res.json(resposta);
+                            } else {
+                                resposta["erro"] = 1;
+                                resposta["dados"] = "Email e senha não compátíveis";
+                                res.json(resposta);
+                            }
+                        });
+                    } else {
+                        resposta["erro"] = 1;
+                        resposta["dados"] = "Email não existente";
+                        res.json(resposta);
+                    }
+                }
+            });
+            connection.release();
         }
-        /*else if(){
-    
-        }*/
-        console.error(result);
-        res.send("sucesso");
-    })
+    });
 });
+
+
+/*  Middleware para validar o token (JWT).
+    Requisições antes daqui não precisam de token;
+    Todas as requisições após essa função precisarão ter o token validado
+    para serem acessadas.    */
+users.use(function (req, res, next) {
+    var token = req.body.token || req.headers['token'];
+    var resposta = {};
+    if (token) {
+        jwt.verify(token, process.env.SECRET_KEY, function (err) {
+            if (err) {
+                resposta["erro"] = 1;
+                resposta["dados"] = "Token inválido";
+                res.json(resposta);
+            } else {
+                next();
+            }
+        });
+    } else {
+        resposta["erro"] = 1;
+        resposta["dados"] = "É necessário um token para acessar essa função";
+        res.json(resposta);
+    }
+});
+
+users.put('/atualizarPerfil/:userID', function (req, res) {
+    // a implementar
+});
+
 
 module.exports = users;
