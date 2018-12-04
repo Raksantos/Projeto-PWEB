5b40290e3875486b993a11b133a788450682be83
Joao Falcao <joao.falcao@icloud.com>
2018-12-04 01:40:21 -0300

    porta servidor

--- backend/servidor.js
+++ backend/servidor.js
@@ -23,6 +23,6 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(8000, function () {
-    console.log('Servidor ouvindo na porta: 8000');
+app.listen(process.env.PORT || 8000, function () {
+    console.log('Servidor ouvindo na porta: '+process.env.PORT || 8000);
 });
