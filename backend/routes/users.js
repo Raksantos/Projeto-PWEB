1d7032b1bd15df37fcff9f529a294ee9eccc606e
Joao Falcao <joao.falcao@icloud.com>
2018-12-04 00:42:07 -0300

    liberando rota para perfil jogo

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -225,6 +225,120 @@ users.get('/perfil/:userID', function(req, res){
     });
 });
 
+users.get('/perfisJogo/:userID', function (req, res) {
+    var userID = req.params.userID;
+    console.log(userID);
+
+    var resposta = {
+        "erro": 1,
+        "dados": ""
+    };
+    var aux = [];
+    database.connection.getConnection(function (err, connection) {
+        if (err) {
+            resposta["erro"] = 1;
+            resposta["dados"] = "Erro interno do servidor";
+            res.json(resposta);
+        } else {
+            connection.query('SELECT * FROM t_usuario_jogo WHERE id_usuario = ?', userID, function (err, rows, fields) {
+                if (err)
+                    throw err;
+                else if (rows.length > 0) {
+                    console.log(rows);
+                    for (var i = 0, len = rows.length; i < len; i++) {
+                        var e = rows[i];
+                        var params;
+                        var sql;
+                        if (e.id_mapa != null) {
+                            params = [userID, e.id_jogo, e.id_jogo, e.id_rank, e.id_funcao, e.id_mapa];
+                            sql = 'SELECT f.nome AS funcao, j.nome AS jogo, r.nome AS rank, m.nome AS mapa, u.nickname ';
+                            sql += 'FROM t_funcao f, t_jogo j, t_rank r, t_mapa m, t_usuario_jogo u ';
+                            sql += 'WHERE u.id_usuario=? AND u.id_jogo=? AND j.id=? AND r.id=? AND f.id=? AND m.id=?';
+                        }
+                        else {
+                            params = [userID, e.id_jogo, e.id_jogo, e.id_rank, e.id_funcao];
+                            sql = 'SELECT f.nome AS funcao, j.nome AS jogo, r.nome AS rank, u.nickname ';
+                            sql += 'FROM t_funcao f, t_jogo j, t_rank r, t_usuario_jogo u ';
+                            sql += 'WHERE u.id_usuario=? AND u.id_jogo=? AND j.id=? AND r.id=? AND f.id=?';
+                        }
+                        connection.query(sql, params, function (err, result) {
+                            if (err)
+                                throw err;
+                            else {
+                                console.log(result);
+                                aux.push(result);
+                            }
+                        });
+                    }
+                    resposta["erro"] = 0;
+                    resposta["dados"] = rows;
+                    res.json(resposta);
+                } else {
+                    resposta["dados"] = "Nenhum dado encontrado";
+                    res.json(resposta);
+                }
+            });
+            connection.release();
+        }
+    })
+});
+
+users.get('/perfilJogo/:userID/:jogoID', function (req, res) {
+    var userID = req.params.userID;
+    var jogoID = req.params.jogoID;
+    var params = [userID, jogoID];
+
+    var resposta = {
+        "erro": 1,
+        "dados": ""
+    };
+
+    database.connection.getConnection(function (err, connection) {
+        if (err) {
+            resposta["erro"] = 1;
+            resposta["dados"] = "Erro interno do servidor";
+            res.json(resposta);
+        } else {
+            connection.query('SELECT * FROM t_usuario_jogo WHERE id_usuario = ? AND id_jogo = ?', params, function (err, rows, fields) {
+                if (err)
+                    throw err;
+                else if (rows.length > 0) {
+                    console.log(rows);
+                    var e = rows[0];
+                    var params2;
+                    var sql;
+                    if (e.id_mapa != null) {
+                        params2 = [userID, e.id_jogo, e.id_jogo, e.id_rank, e.id_funcao, e.id_mapa];
+                        sql = 'SELECT f.nome AS funcao, j.nome AS jogo, r.nome AS rank, r.id AS id_rank, m.nome AS mapa, u.nickname ';
+                        sql += 'FROM t_funcao f, t_jogo j, t_rank r, t_mapa m, t_usuario_jogo u ';
+                        sql += 'WHERE u.id_usuario=? AND u.id_jogo=? AND j.id=? AND r.id=? AND f.id=? AND m.id=?';
+                    }
+                    else {
+                        params2 = [userID, e.id_jogo, e.id_jogo, e.id_rank, e.id_funcao];
+                        sql = 'SELECT f.nome AS funcao, j.nome AS jogo, r.nome AS rank, r.id AS id_rank, u.nickname ';
+                        sql += 'FROM t_funcao f, t_jogo j, t_rank r, t_usuario_jogo u ';
+                        sql += 'WHERE u.id_usuario=? AND u.id_jogo=? AND j.id=? AND r.id=? AND f.id=?';
+                    }
+                    connection.query(sql, params2, function (err, result) {
+                        if (err)
+                            throw err;
+                        else {
+                            console.log(result);
+                            resposta["erro"] = 0;
+                            resposta["dados"] = result;
+                            res.json(resposta);
+                        }
+                    });
+                } else {
+                    resposta["dados"] = "Nenhum dado encontrado";
+                    res.json(resposta);
+                }
+            });
+            connection.release();
+        }
+    });
+});
+
 //  Middleware para validar o token (JWT).
 //  Requisições antes daqui não precisam de token;
 //  Todas as requisições após essa função precisarão ter o token validado
@@ -422,119 +536,5 @@ users.get('/listarHorario/:userID', function (req, res) {
         }
     })
 })
-
-users.get('/perfisJogo/:userID', function (req, res) {
-    var userID = req.params.userID;
-    console.log(userID);
-
-    var resposta = {
-        "erro": 1,
-        "dados": ""
-    };
-    var aux = [];
-    database.connection.getConnection(function (err, connection) {
-        if (err) {
-            resposta["erro"] = 1;
-            resposta["dados"] = "Erro interno do servidor";
-            res.json(resposta);
-        } else {
-            connection.query('SELECT * FROM t_usuario_jogo WHERE id_usuario = ?', userID, function (err, rows, fields) {
-                if (err)
-                    throw err;
-                else if (rows.length > 0) {
-                    console.log(rows);
-                    for (var i = 0, len = rows.length; i < len; i++) {
-                        var e = rows[i];
-                        var params;
-                        var sql;
-                        if (e.id_mapa != null) {
-                            params = [userID, e.id_jogo, e.id_jogo, e.id_rank, e.id_funcao, e.id_mapa];
-                            sql = 'SELECT f.nome AS funcao, j.nome AS jogo, r.nome AS rank, m.nome AS mapa, u.nickname ';
-                            sql += 'FROM t_funcao f, t_jogo j, t_rank r, t_mapa m, t_usuario_jogo u ';
-                            sql += 'WHERE u.id_usuario=? AND u.id_jogo=? AND j.id=? AND r.id=? AND f.id=? AND m.id=?';
-                        }
-                        else {
-                            params = [userID, e.id_jogo, e.id_jogo, e.id_rank, e.id_funcao];
-                            sql = 'SELECT f.nome AS funcao, j.nome AS jogo, r.nome AS rank, u.nickname ';
-                            sql += 'FROM t_funcao f, t_jogo j, t_rank r, t_usuario_jogo u ';
-                            sql += 'WHERE u.id_usuario=? AND u.id_jogo=? AND j.id=? AND r.id=? AND f.id=?';
-                        }
-                        connection.query(sql, params, function (err, result) {
-                            if (err)
-                                throw err;
-                            else {
-                                console.log(result);
-                                aux.push(result);
-                            }
-                        });
-                    }
-                    resposta["erro"] = 0;
-                    resposta["dados"] = rows;
-                    res.json(resposta);
-                } else {
-                    resposta["dados"] = "Nenhum dado encontrado";
-                    res.json(resposta);
-                }
-            });
-            connection.release();
-        }
-    })
-});
-
-users.get('/perfilJogo/:userID/:jogoID', function (req, res) {
-    var userID = req.params.userID;
-    var jogoID = req.params.jogoID;
-    var params = [userID, jogoID];
-
-    var resposta = {
-        "erro": 1,
-        "dados": ""
-    };
-
-    database.connection.getConnection(function (err, connection) {
-        if (err) {
-            resposta["erro"] = 1;
-            resposta["dados"] = "Erro interno do servidor";
-            res.json(resposta);
-        } else {
-            connection.query('SELECT * FROM t_usuario_jogo WHERE id_usuario = ? AND id_jogo = ?', params, function (err, rows, fields) {
-                if (err)
-                    throw err;
-                else if (rows.length > 0) {
-                    console.log(rows);
-                    var e = rows[0];
-                    var params2;
-                    var sql;
-                    if (e.id_mapa != null) {
-                        params2 = [userID, e.id_jogo, e.id_jogo, e.id_rank, e.id_funcao, e.id_mapa];
-                        sql = 'SELECT f.nome AS funcao, j.nome AS jogo, r.nome AS rank, r.id AS id_rank, m.nome AS mapa, u.nickname ';
-                        sql += 'FROM t_funcao f, t_jogo j, t_rank r, t_mapa m, t_usuario_jogo u ';
-                        sql += 'WHERE u.id_usuario=? AND u.id_jogo=? AND j.id=? AND r.id=? AND f.id=? AND m.id=?';
-                    }
-                    else {
-                        params2 = [userID, e.id_jogo, e.id_jogo, e.id_rank, e.id_funcao];
-                        sql = 'SELECT f.nome AS funcao, j.nome AS jogo, r.nome AS rank, r.id AS id_rank, u.nickname ';
-                        sql += 'FROM t_funcao f, t_jogo j, t_rank r, t_usuario_jogo u ';
-                        sql += 'WHERE u.id_usuario=? AND u.id_jogo=? AND j.id=? AND r.id=? AND f.id=?';
-                    }
-                    connection.query(sql, params2, function (err, result) {
-                        if (err)
-                            throw err;
-                        else {
-                            console.log(result);
-                            resposta["erro"] = 0;
-                            resposta["dados"] = result;
-                            res.json(resposta);
-                        }
-                    });
-                } else {
-                    resposta["dados"] = "Nenhum dado encontrado";
-                    res.json(resposta);
-                }
-            });
-            connection.release();
-        }
-    });
-});
 
 module.exports = users;
